383b04f36555f56138787b019fe3b484ca9bf933
Noshin-khan <noshinkhan877@gmail.com>
2023-09-17 16:58:42 +0530

    First upload

---
+++ khan.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import polymorphism.noshin;
+class khan
+{
+    public static void main(String[]args)
+    {  /*  char u,l;
+    int ascii;
+    Scanner scan=new Scanner(System.in);
+    System.out.println("enter the upper case");
+    u=scan.next().charAt(0);
+    ascii=u+32;
+    l=(char)ascii;
+    System.out.println(l);*/
+   noshin n=new noshin();
+   n.disp();
+        
+        
+
+
+    }
+}
